9c5bc24a6375b4d9752c8170a46d626cf76d0f67
100001522902 <violincorn@gmail.com>
2019-03-23 18:33:18 +0800

    fix picture width in 404 page

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -4,6 +4,6 @@
     <p>
       这里还在施工呢……不如<a href="http://home.ustc.edu.cn/~violinwang/donate/">捐助作者</a>让他更努力工作吧！
     </p>
-    <img src="static/dev.png" class="img-rounded img-responsive" />
+    <img src="static/dev.png" class="img-rounded img-responsive" style="width:100%" />
   </div>
 </template>
