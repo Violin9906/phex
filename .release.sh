7e306b3584415c3216d9a715b1896ad70e756ed6
violin <violinwang@mail.ustc.edu.cn>
2019-12-06 20:01:31 +0800

    fix release.sh

--- .release.sh
+++ .release.sh
@@ -3,11 +3,11 @@
 export PKG_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 git tag $PKG_VERSION
 if [ $? -eq 0 ]; then
-    export RELEASE = true
-    if [$PKG_VERSION =~ /\.0/]; then
-        export PRE_RELEASE = false
+    export RELEASE=true
+    if [ $PKG_VERSION =~ /\.0/ ]; then
+        export PRE_RELEASE=false
     else
-        export PRE_RELEASE = true
+        export PRE_RELEASE=true
     fi
     tar -czvf $PKG_VERSION.tar.gz ./dist/*
 fi
